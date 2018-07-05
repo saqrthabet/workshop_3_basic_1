95c608bf34774f30f92735c0c1e541fe34cbfa9b
SAQR THABET <saqrthabet2004@yahoo.com>
2018-07-05 19:10:16 +0800

    convert all to classes

--- main.py
+++ main.py
@@ -3,15 +3,17 @@
     This is the main application file for the Hotel Reservation system 
 """
 
-import tester
+from tester import Tester
       
 """
 Main working platform
 """
-tester.start_app()
-
-"""
 def start_app():
-    test_app = tester.Tester()
+    test_app = Tester()
     test_app.fill_test()
+
+start_app()
+
+"""  
+    tester.start_app()
 """
